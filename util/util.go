a1b37a764d0354c072bca457a8ec3760f68bf981
BKR <bkr@groundfloor.digital>
2022-08-08 22:15:08 +0200

    building reliable execComand

--- util/util.go
+++ util/util.go
@@ -1,9 +1,22 @@
 package util
 
+import (
+	"errors"
+	"os/exec"
+)
+
 func DownloadFile() error {
 	return nil
 }
 
 func ExecCommand(cmdName string, args []string) error {
+	cmd := exec.Command(cmdName)
+	err := cmd.Run()
+	if errors.Is(cmd.Err, exec.ErrDot) {
+		cmd.Err = nil
+	}
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 	return nil
 }
